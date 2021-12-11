cd2e79a1ec2a4e1e3ff67719a1bb59988cb8bd84
Scindapsus345 <zavnikita345@gmail.com>
2021-12-11 18:32:54 +0500

    Add Xamarin project

---
+++ PowerWaiter/PowerWaiter/PowerWaiter/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PowerWaiter.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PowerWaiter.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
