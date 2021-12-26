349fe601179464089ee407e342943121a3c2ea81
Scindapsus345 <zavnikita345@gmail.com>
2021-12-26 23:49:03 +0500

    Fix Achievements

--- PowerWaiters/PowerWaiters/PowerWaiters/Models/AchievementModel.cs
+++ PowerWaiters/PowerWaiters/PowerWaiters/Models/AchievementModel.cs
@@ -7,11 +7,12 @@
         public string IconUrl { get; set; }
         public int Purpose { get; set; }
         public int Progress { get; set; }
+        public int Level { get; set; }
         public bool Completed => Purpose == Progress;
         public string ProgressString => Completed ? "Получена" : $"{Progress} / {Purpose}";
         public string BorderColor => Completed ? "Black" : "Transparent";
         public string TextColor => Completed ? "Black" : "#999999";
-        public string ProgressTextColor => Completed ? "ForestGreen" : "Black";
+        public string LevelString => $"Уровень: {Level}";
         public string BackgroundColor => Completed ? "#F0FFF0" : "#F1F1F1";
     } 
 }
